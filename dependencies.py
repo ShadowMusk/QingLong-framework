13183f6a85f8ee4bf5d532d2884889482a79a5b4
ShadowMusk <103261784+ShadowMusk@users.noreply.github.com>
2023-08-08 17:47:24 +0800

    Framework Dependencies（安装框架所需依赖）

--- dependencies.py
+++ dependencies.py
@@ -8,13 +8,13 @@ os.system("pip3 install -r myrequirements.txt")
 def install(tool_name):
     # 先尝试使用 apt-get 来安装
     apt_get_result = os.system("sudo apt-get install {}".format(tool_name))
-    if apt_get_result == 0:  # apt-get 命令成功执行
+    if apt_get_result == 0:  # apt-get 命令成功执行.
         return
     # 如果 apt-get 安装失败，再尝试使用 yum 来安装
     yum_result = os.system("sudo yum install {}".format(tool_name))
     if yum_result == 0:  # yum 命令成功执行
         return
-    # 如果 yum 安装失败，打印错误信息
+    # 如果 yum 安装失败，打印错误信息.
     print('\033[1;32;32m' + "[-] Failed to install {} with both apt-get and yum.".format(tool_name) + '\033[0m')
 
 
