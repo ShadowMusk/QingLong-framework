e14dd00a6aaa82044ac8fc55704bf28c8112c5fa
ShadowMusk <103261784+ShadowMusk@users.noreply.github.com>
2023-08-14 15:11:20 +0800

    updated index.py

--- index.py
+++ index.py
@@ -1,6 +1,7 @@
 from prompt_toolkit import prompt
 from prompt_toolkit.completion import WordCompleter
 from prompt_toolkit.formatted_text import ANSI
+from prompt_toolkit.history import InMemoryHistory
 import sys
 import logo
 import QingLong
@@ -10,9 +11,10 @@ if __name__ == '__main__':
     commands = ["let's start", "quit"]
     completer = WordCompleter(commands)
     formatted_text = ANSI('\033[1;32;32m(QingLong Framework) > \033[0m')
+    history = InMemoryHistory()
     logo.logo()
     while True:
-        choice = prompt(formatted_text, completer=completer)
+        choice = prompt(formatted_text, completer=completer, history=history)
         if choice == "let's start":
             qinglong = QingLong.QingLong(thread_session)
             break
