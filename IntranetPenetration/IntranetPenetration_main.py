99e9510ccefd851c9ded549cd57ff898628d9f75
ShadowMusk <103261784+ShadowMusk@users.noreply.github.com>
2023-08-14 09:37:21 +0800

    Intranet Penetration（内网渗透模块）:updated IntranetPenetration_main.py

--- IntranetPenetration/IntranetPenetration_main.py
+++ IntranetPenetration/IntranetPenetration_main.py
@@ -29,16 +29,22 @@ class IntranetPenetration:
                 self.show_params()
                 continue
             elif choice == '1':
-                print("Please turn to readme.md")
+                print("Please turn to README.md")
                 continue
             # 生成后门
             elif choice.split()[0] == '2':
-                backdoor = attacker.Attacker(choice)
-                print('\033[1;32;32m' + "[+] Backdoor successfully generated!" + '\033[0m')
-                print('\033[1;32;32m' + "[+] Type \"show functions\" to learn more details about the backdoor." + '\033[0m')
-                # 添加启动后门
-                self.thread_session.append(backdoor)
-                continue
+                try:
+                    backdoor = attacker.Attacker(choice)
+                    print('\033[1;32;32m' + "[+] Backdoor successfully generated!" + '\033[0m')
+                    print('\033[1;32;32m' + "[+] Type \"show functions\" to learn more details about the backdoor." + '\033[0m')
+                    # 添加启动后门
+                    self.thread_session.append(backdoor)
+                    continue
+                except KeyboardInterrupt:
+                    print("exiting......")
+                except OSError:
+                    print('\033[1;31;31m' + "[-] The port is currently occupied.Please try using a different port.Or try again later." + '\033[0m')
+                    continue
             # 查看所有会话
             elif choice == 'show sessions':
                 headers = ['\033[1;34;34m' + "id" + '\033[0m', '\033[1;34;34m' + "local ip" + '\033[0m', '\033[1;34;34m' + "public ip" + '\033[0m', '\033[1;34;34m' + "user" + '\033[0m', '\033[1;34;34m' + "hostname" + '\033[0m', '\033[1;34;34m' + "system info" + '\033[0m']
@@ -62,6 +68,9 @@ class IntranetPenetration:
                 except IndexError:
                     print('\033[1;31;31m' + "[-] No such session!" + '\033[0m')
                     continue
+                except ValueError:
+                    print('\033[1;31;31m' + "[-] wrong command!" + '\033[0m')
+                    continue
             # 删除已有会话
             elif choice.split()[0] == 'delete':
                 try:
@@ -73,11 +82,14 @@ class IntranetPenetration:
                 except IndexError:
                     print('\033[1;31;31m' + "[-] Failed to delete the session because it does not exit." + '\033[0m')
                     continue
+                except ValueError:
+                    print('\033[1;31;31m' + "[-] wrong command!" + '\033[0m')
+                    continue
 
     def show_params(self):
         print("\n" + '\033[1;34;34m' + "Parameter Description" + '\033[0m' + "\n" + '=' * len("Parameter Description") + "\n")
         headers = ['\033[1;34;34m' "Params" + '\033[0m', '\033[1;34;34m' "Description" + '\033[0m']
-        mydata = [["attacker_ip", "攻击者者IP"], ["port", "监听端口"], ["session_id", "后门的id"]]
+        mydata = [["attacker_ip", "攻击者IP"], ["port", "监听端口"], ["session_id", "后门的id"]]
         MyTable.createTable(headers, mydata)
 
     def show_functions(self):
